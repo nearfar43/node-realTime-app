5a1e9648fa6bba696b7e0c00d6f6f5ec819df3b5
nearfar43 <nearfar@rams.colostate.edu>
2018-08-05 15:04:04 -0700

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -17,9 +17,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'jenny',
+		text: 'hello world',
+		createdAt: 11123
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
+
 });
 
 
